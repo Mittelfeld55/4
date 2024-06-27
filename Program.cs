cec355a9bcf46b67cfc853e183034a466cbc40b6
Mittelfeld55 <Mittelfeld55@gmail.com>
2024-06-27 11:25:23 +0200

    Converted to different style - Kartenziehen -> Mischalgorithmus

--- Program.cs
+++ Program.cs
@@ -1,89 +1,90 @@
-﻿#pragma warning disable CS0219
-var blatt = "A1A2A3A4B1B2B3B4C1C2C3C4D1D2D3D4E1E2E3E4F1F2F3F4G1G2G3G4H1H2H3H4";
-Console.WriteLine("Blatt: " + blatt + " " + blatt.Length/2);
-int anzahlKartenImBlatt = blatt.Length / 2;
-string kartenDesSpielers = "";
-string stapelB = "";
-Random rnd = new Random();
+﻿
+using System;
 
-var pPunkte = 0;
-var cPunkte = 0;
-
-for (int i = 0; i < 14; i++)
+class Kartenspiel
 {
-    var randomKarte = rnd.Next(0, anzahlKartenImBlatt);
-    int startIndex = randomKarte * 2;
-    string karte = blatt.Substring(startIndex, 2);
-    blatt = blatt.Remove(startIndex, 2);
-    anzahlKartenImBlatt--;
-    if (i % 2 == 0)
+    public void SpielStarten()
     {
-        kartenDesSpielers += karte;
-    }
-    else
-    {
-        stapelB += karte;
+        #pragma warning disable CS0219 // Unterdrückt die Warnung für ungenutzte Variablen
+        
+        var blatt = "A1A2A3A4B1B2B3B4C1C2C3C4D1D2D3D4E1E2E3E4F1F2F3F4G1G2G3G4H1H2H3H4";
+        Console.WriteLine("Blatt: " + blatt + " " + blatt.Length / 2);
+        int anzahlKartenImBlatt = blatt.Length / 2;
+        string kartenDesSpielers = "";
+        string stapelB = "";
+        Random rnd = new Random();
+
+        // Use the method from the separate class file
+        KartenOperations.VerteileKarten(ref blatt, ref anzahlKartenImBlatt, ref kartenDesSpielers, ref stapelB, rnd);
+
+        Console.Write("\nDeine Karten : " + kartenDesSpielers + " " + kartenDesSpielers.Length / 2 + " |      Karten des Gegners: " + stapelB + " " + stapelB.Length / 2);
+
+        // Nach Karten Fragen
+        int y = 15;
+        for (int i = 0; i < y; i++)
+        {
+            Console.WriteLine("\nWelche Karte brauchst? (z.B. A1)	");
+            var benoetigteKarte = (Console.ReadLine()?.Trim().ToUpper() ?? " ").Substring(0, 2);
+            if (benoetigteKarte == " " || benoetigteKarte.Length > 2)
+            {
+                Console.WriteLine(" Error! Du hast keine oder mehr als 2 Karten eingegeben.");
+            }
+            else
+            {
+                if (stapelB.Contains(benoetigteKarte))
+                {
+                    Console.WriteLine("Der Gegner hat die Karte " + benoetigteKarte);
+                    stapelB = stapelB.Replace(benoetigteKarte, "");
+                    kartenDesSpielers += benoetigteKarte;
+                    Console.Write("\nDeine Karten : " + kartenDesSpielers + " " + kartenDesSpielers.Length / 2 + " |      Karten des Gegners: " + stapelB + " " + stapelB.Length / 2);
+                }
+                else
+                {
+                    Console.WriteLine("Der Gegner hat die Karte " + benoetigteKarte + " nicht.");
+                    var Karteziehen = rnd.Next(0, anzahlKartenImBlatt);
+                    int startIndex = Karteziehen * 2;
+                    string gezogeneKartekarte = blatt.Substring(startIndex, 2);
+                    Console.WriteLine("Du hast die Karte " + gezogeneKartekarte + " gezogen.");
+                    blatt = blatt.Remove(startIndex, 2);
+                    kartenDesSpielers += gezogeneKartekarte;
+                }
+                string zieheBlatt = "A1A2A3A4B1B2B3B4C1C2C3C4D1D2D3D4E1E2E3E4F1F2F3F4G1G2G3G4H1H2H3H4";
+                string SubstringToRemove = stapelB;
+                var zieheBlattLength = zieheBlatt.Length / 2;
+                var nachKarteFragen = rnd.Next(0, zieheBlattLength);
+                int angefragteKarte = nachKarteFragen * 2;
+                string gefragteKarte = blatt.Substring(angefragteKarte, 2);
+                Console.WriteLine("\nDer Computer hat nach: " + gefragteKarte + " gefragt.");
+                if (kartenDesSpielers.Contains(gefragteKarte))
+                {
+                    Console.WriteLine("Du hattest die Karte " + gefragteKarte);
+                    kartenDesSpielers = kartenDesSpielers.Replace(gefragteKarte, " ");
+                    Console.Write("\nDeine Karten : " + kartenDesSpielers + " " + kartenDesSpielers.Length / 2);
+                    Console.WriteLine("|      Karten des Gegners: " + stapelB + " " + stapelB.Length / 2);
+                }
+                else
+                {
+                    Console.WriteLine("\nDu hast die Karte " + gefragteKarte + " nicht.");
+                    var Karteziehen = rnd.Next(0, anzahlKartenImBlatt);
+                    int startIndex = Karteziehen * 2;
+                    string gezogeneKartekarte = blatt.Substring(startIndex, 2);
+                    Console.WriteLine("Der Computer hat " + gezogeneKartekarte + " gezogen.");
+                    blatt = blatt.Remove(startIndex, 2);
+                    stapelB += gezogeneKartekarte;
+                    Console.Write("\nDeine Karten : " + kartenDesSpielers + " " + kartenDesSpielers.Length / 2 + " |      Karten des Gegners: " + stapelB + " " + stapelB.Length / 2);
+                }
+            }
+        }
+        
+        #pragma warning restore CS0219 // Hebt die Unterdrückung der Warnung auf
     }
 }
-Console.Write("\nDeine Karten : " + kartenDesSpielers + " " + kartenDesSpielers.Length/2);
-Console.WriteLine("      |      Karten des Gegners: " + stapelB + " " + stapelB.Length/2);
 
-//Nach Karten Fragenx
-int y = 15;
-for (int i = 0; i < y; i++)
+class Program
 {
-    Console.WriteLine("\nWelche Karte brauchst? (z.B. A1)	");
-    var benoetigteKarte = Console.ReadLine()?.Trim().ToUpper() ?? " ";
-    benoetigteKarte = benoetigteKarte.Substring(0, 2);
-    if (benoetigteKarte == " " || benoetigteKarte.Length > 2) 
+    static void Main(string[] args)
     {
-        Console.WriteLine(" Error! Du hast keine oder 2 Karten eingegeben.");
-        
+        Kartenspiel spiel = new Kartenspiel();
+        spiel.SpielStarten();
     }
-    else
-    {
-        if (stapelB.Contains(benoetigteKarte))
-        {
-            Console.WriteLine("Der Gegner hat die Karte " + benoetigteKarte);
-            stapelB = stapelB.Replace(benoetigteKarte, "");
-            kartenDesSpielers += benoetigteKarte;
-            Console.Write("\nDeine Karten : " + kartenDesSpielers + " " + kartenDesSpielers.Length/2 + " |      Karten des Gegners: " + stapelB + " " + stapelB.Length/2);
-        }
-        else
-        {
-            Console.WriteLine("Der Gegner hat die Karte " + benoetigteKarte + " nicht.");
-            var Karteziehen = rnd.Next(0, anzahlKartenImBlatt);
-            int startIndex = Karteziehen * 2;
-            string gezogeneKartekarte = blatt.Substring(startIndex, 2);
-            Console.WriteLine("Du hast die Karte " + gezogeneKartekarte + " gezogen.");
-            blatt = blatt.Remove(startIndex, 2);
-            kartenDesSpielers += gezogeneKartekarte;
-        }
-        string zieheBlatt = "A1A2A3A4B1B2B3B4C1C2C3C4D1D2D3D4E1E2E3E4F1F2F3F4G1G2G3G4H1H2H3H4";
-        string SubstringToRemove = stapelB;
-        var zieheBlattLength = zieheBlatt.Length / 2;
-        var nachKarteFragen = rnd.Next(0, zieheBlattLength);
-        int angefragteKarte = nachKarteFragen * 2;
-        string gefragteKarte = blatt.Substring(angefragteKarte, 2);
-        Console.WriteLine("\nDer Computer hat nach: " + gefragteKarte + " gefragt.");
-        if (kartenDesSpielers.Contains(gefragteKarte)) 
-        {
-            Console.WriteLine("Du hattest die Karte " + gefragteKarte );
-            kartenDesSpielers = kartenDesSpielers.Replace(gefragteKarte, " ");
-            Console.Write("\nDeine Karten : " + kartenDesSpielers + " " + kartenDesSpielers.Length/2);
-            Console.WriteLine("|      Karten des Gegners: " + stapelB + " " + stapelB.Length/2);
-        }
-        else 
-        {
-            Console.WriteLine("\nDu hast die Karte " + gefragteKarte + " nicht.");
-            var Karteziehen = rnd.Next(0, anzahlKartenImBlatt);
-            int startIndex = Karteziehen * 2;
-            string gezogeneKartekarte = blatt.Substring(startIndex, 2);
-            Console.WriteLine("Der Computer hat " + gezogeneKartekarte + " gezogen.");
-            blatt = blatt.Remove(startIndex, 2);
-            stapelB += gezogeneKartekarte;
-            Console.Write("\nDeine Karten : " + kartenDesSpielers + " " + kartenDesSpielers.Length/2 + " |      Karten des Gegners: " + stapelB + " " + stapelB.Length/2);
-        }
-    }
-  
 }
