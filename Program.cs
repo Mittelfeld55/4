9bca13c22ea25073ddbc583f9c48f64134d2ffcd
Mittelfeld55 <Mittelfeld55@gmail.com>
2024-06-28 11:54:20 +0200

    Strukturänderungen

--- Program.cs
+++ Program.cs
@@ -59,14 +59,13 @@ for (int i = 0; i < y; i++)
                 kartenDesSpielers += gezogeneKartekarte;
             }
         }
-<<<<<<< HEAD
         cZ = !cZ;
         sZ = !sZ;
-=======
+
         cZ = true;
         sZ = false;
-        // test commit
->>>>>>> 22c77a49376575af7fa88e64140ab717b20ee8f8
+        
+
     }
     else if (cZ & !sZ) { //Gegner Zug
         string zieheBlatt = "A1A2A3A4B1B2B3B4C1C2C3C4D1D2D3D4E1E2E3E4F1F2F3F4G1G2G3G4H1H2H3H4";
